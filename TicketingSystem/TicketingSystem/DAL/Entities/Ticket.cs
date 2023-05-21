791d9470526da35af10098112f7dd42910f1d42e
VALENTINA ALVAREZ PANIAGUA <valentinaalvarez305940@correo.itm.edu.co>
2023-05-21 18:21:32 -0500

    New class called SeederDb

--- TicketingSystem/TicketingSystem/DAL/Entities/Ticket.cs
+++ TicketingSystem/TicketingSystem/DAL/Entities/Ticket.cs
@@ -7,17 +7,17 @@ namespace TicketingSystem.DAL.Entities
     {
 
 
-        [Display(Name = "Use Date of the ticket")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [Display(Name = "Use Date of the ticket")] //show by UI
         [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
-        public DateTime UseDate { get; set;}
+        public DateTime? UseDate { get; set;}
 
-        [Display(Name = "It was used? ")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [Display(Name = "It was used? ")] //show by UI
         [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
         public bool IsUsed { get; set;}
 
-        [Display(Name = "Porter's lodge")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [Display(Name = "Porter's lodge")] //show by UI
         [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
-        public string EntranceGate { get; set;}
+        public string? EntranceGate { get; set;}
 
     }
 
---
+++ TicketingSystem/TicketingSystem/DAL/SeederDb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketingSystem.DAL.Entities;
+
+namespace TicketingSystem.DAL
+{
+    public class SeederDb
+    {
+        private readonly ConcertDB_ValentinaAlvarez _context;
+        public SeederDb(ConcertDB_ValentinaAlvarez context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync(); //This line helps me create my database automatically
+            await PopulateTicketsAsync();
+           
+
+            await _context.SaveChangesAsync();
+        }
+
+            private async Task PopulateTicketsAsync()
+            {
+                for (int i = 0; i < 50000; i++)
+                {
+                    Ticket ticket = new Ticket
+                    {
+                        UseDate = null,
+                        IsUsed = false,
+                        EntranceGate = null
+                    };
+
+                    _context.Tickets.Add(ticket);
+                }
+            }
+
+        
+    }
+}
