d818a3ee4d1a1e2c31bdc5855e892936dfeecc85
VALENTINA ALVAREZ PANIAGUA <valentinaalvarez305940@correo.itm.edu.co>
2023-05-21 13:41:37 -0500

    Creating the entity class

---
+++ TicketingSystem/TicketingSystem/DAL/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketingSystem.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+    }
+}
---
+++ TicketingSystem/TicketingSystem/DAL/Entities/Tickets.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TicketingSystem.DAL.Entities
+{
+    public class Tickets : Entity
+    {
+        [Display(Name = "UseDate")] //Fecha de uso de la boleta
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] //LONGITUD MÁXIMA (NVARCHAR(50))
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
+        public string Name { get; set; }
+
+        [Display(Name = "Estado")]
+        public ICollection<State> States { get; set; } //Esto significa que un país puede tener N Estados    
+
+        //Propiedad de Lectura que no se mapea en la tabla de la DB
+        public int StatesNumber => States == null ? 0 : States.Count; // If Ternario (? Entonces), (: sino)
+    }
+}
