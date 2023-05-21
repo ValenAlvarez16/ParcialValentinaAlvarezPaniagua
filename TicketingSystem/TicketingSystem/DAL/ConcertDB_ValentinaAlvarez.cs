2a3a2710b1ef0f6237796babe2c7a6d0fe5e0184
VALENTINA ALVAREZ PANIAGUA <valentinaalvarez305940@correo.itm.edu.co>
2023-05-21 15:25:29 -0500

    New ConcertDB class and Ticket

---
+++ TicketingSystem/TicketingSystem/DAL/ConcertDB_ValentinaAlvarez.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using TicketingSystem.DAL.Entities;
+
+namespace TicketingSystem.DAL
+{
+    public class ConcertDB_ValentinaAlvarez : DbContext
+    {
+        public ConcertDB_ValentinaAlvarez(DbContextOptions<ConcertDB_ValentinaAlvarez> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(c => c.Id).IsUnique();
+        }
+    }
+}   
---
+++ TicketingSystem/TicketingSystem/DAL/Entities/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TicketingSystem.DAL.Entities
+{
+    public class Tickets : Entity
+    {
+
+
+        [Display(Name = "Use Date of the ticket")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
+        public DateTime? UseDate { get; set;}
+
+        [Display(Name = "It was used? ")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
+        public bool IsUsed { get; set;}
+
+        [Display(Name = "Porter's lodge")] //ASÍ ES COMO SE VA A MOSTRAR POR UI
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //NOT NULL
+        public string EntranceGate { get; set;}
+
+    }
+
+}
